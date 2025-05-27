bbcd3e64f2bbf5f8a7da3b0c5136cab89c58dffa
Elif Alptekin <ealptekina@gmail.com>
2025-05-27 22:08:43 +0300

    Add project files.

---
+++ BlogStore.DataAccessLayer/Context/BlogContext.cs
@@ -0,0 +1,20 @@
+﻿using BlogStore.EntityLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogStore.DataAccessLayer.Context
+{
+    public class BlogContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // SQL Server bağlantı
+            optionsBuilder.UseSqlServer("Server=DESKTOP-IMEIVGC\\SQLEXPRESS;Initial Catalog=BlogStore;Integrated Security=true;Trust Server Certificate=true");
+        }
+
+        // DbSet'ler (tablolar) tanımlanır
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Article> Articles { get; set; }
+    }
+}
